aa7aa97f6f2889c6238ea2f17e8f5abcf9c5799a
Chris Barrett <cmbarrett@gmail.com>
2013-03-08 18:07:02 +0100

    adding static version for SEO

---
+++ app/js/static.js
@@ -0,0 +1,103 @@
+$(document).ready(function() {
+    $(".localeSelector").on('change', function() {
+        changeLocale(this.value);
+    })
+
+    if(urlParams['q']) {
+        doSearch(urlParams['q']);
+    }
+
+    $('[data-toggle="collapse"]').click(function() {
+        var newText = "";
+        doSearch('');
+
+        switch ($(this).children('.collapserText').text()) {
+            case "More":
+                newText = "Less";
+                break;
+            case "Less":
+                newText = "More";
+                break;
+            case "Plus":
+                newText = "Moins";
+                break;
+            case "Moins":
+                newText = "Plus";
+                break;
+            default:
+                newText = $(this).children('.collapserText').text();
+        }
+        $(this).children('i').toggleClass('icon-chevron-up');
+        $(this).children('i').toggleClass('icon-chevron-down');
+        $(this).children('.collapserText').text(newText);
+    });
+
+    $('.search-query').on('keydown',function(e) {
+        var code = e.keyCode || e.which;
+        if (code == '9' || code == '13')
+        {
+            e.preventDefault();
+            //console.log($(this).val());
+            doSearch($(this).val())
+        }
+    })
+
+    $('#removeFilter').on('click',function() {
+       doSearch('');
+    });
+})
+
+
+
+function doSearch(searchString) {
+    $('.search-query').val(searchString);
+    if(searchString != "") {
+        $('#removeFilter').show();
+        $('#experience1,#experience2,#experience3,#experience4').collapse('show');
+        $('.experience:not(:contains(' + searchString + '))').hide();
+        $('.experience:contains(' + searchString + ')').show();
+        $('.filterable:not(:contains(' + searchString + '))').hide();
+        $('.filterable:contains(' + searchString + ')').show();
+    } else {
+        $('#removeFilter').hide();
+        $('.experience').show()
+        $('.filterable').show();
+    }
+
+}
+
+
+
+function changeLocale(locale) {
+    //get current URL
+    var pathName = window.location.pathname;
+    //construct new URL
+    regEx = new RegExp(/\/(en|fr)\//);
+    pathName = pathName.replace(regEx,"/"+locale+"/");
+    //load correct page
+    window.location.href = pathName;
+}
+
+function getUrlLocale() {
+    var pathName = window.location.pathname;
+    if(pathName.indexOf("/en/") != -1) {
+        return "en";
+    }
+    if(pathName.indexOf("/fr/") != -1) {
+        return "fr";
+    }
+    throw "unknown url locale";
+}
+
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+        urlParams[decode(match[1])] = decode(match[2]);
+})();
